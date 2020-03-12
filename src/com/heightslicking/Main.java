c510e63acf6535f514cf5bd805a455f0de2f7c29
Brandon Gibson <Brandon.gibson.2022@student.lhschools.org>
2020-03-12 14:16:45 -0400

    Ternary Operator built for Hours

--- src/com/heightslicking/Main.java
+++ src/com/heightslicking/Main.java
@@ -5,26 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         ModulusClock();
-
 
     }
 
-
     public static void ModulusClock() {
 
-
         Scanner keyboard;
-        int hour;
-        int minutes;
-
         keyboard = new Scanner(System.in);
-        System.out.println("Give me a amount of hours.");
-        hour = keyboard.nextInt();
-        System.out.println("Give me a amount of minutes.");
-        minutes = keyboard.nextInt();
 
+        System.out.println("Give me a amount of hours in the day.");
+        int hour = keyboard.nextInt();
+        System.out.println("Give me a amount of minutes.");
+        int minutes = keyboard.nextInt();
+
+        hour = minutes >= 60 ? hour + (minutes % 60) : hour;
 
 
     }
