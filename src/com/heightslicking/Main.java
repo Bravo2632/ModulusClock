72ad6803f23766b6ea3e5f9c9f9e1537a5b8f423
Brandon Gibson <Brandon.gibson.2022@student.lhschools.org>
2020-03-12 13:04:17 -0400

    UserDataCaptured

--- src/com/heightslicking/Main.java
+++ src/com/heightslicking/Main.java
@@ -1,8 +1,31 @@
 package com.heightslicking;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        ModulusClock();
+
+
+    }
+
+
+    public static void ModulusClock() {
+
+
+        Scanner keyboard;
+        int hour;
+        int minutes;
+
+        keyboard = new Scanner(System.in);
+        System.out.println("Give me a amount of hours.");
+        hour = keyboard.nextInt();
+        System.out.println("Give me a amount of minutes.");
+        minutes = keyboard.nextInt();
+
+
+
     }
 }
